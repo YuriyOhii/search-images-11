00d44ba2a97ff94394a1280a009bb0dd497ef101
YuriyOhii <uaogiy@gmail.com>
2023-07-27 19:27:04 +0300

    adds pagenation and notifications

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import Pixabay from './js/pixabay-api.js';
 import markupAllCards from './js/card-template.js';
 
+
 const pixabay = new Pixabay();
 
 refs.form.addEventListener('submit', onFormSubmit);
 
 function onFormSubmit(e) {
   e.preventDefault();
-  refs.addMoreBtn.style.display = 'none';
+  refs.notification.style.display = 'none';
   refs.gallery.innerHTML = '';
   pixabay.params.page = 1;
   pixabay.query = e.target.elements.searchQuery.value;
@@ -20,11 +21,21 @@ async function onLoadMoreBtnClick() {
   pixabay.increasePageNumber();
   try {
     const data = await pixabay.fetchData();
+    Notify.success(`Hooray! We found ${data.totalHits} images`);
     const galleryMarkup = markupAllCards(data);
     refs.gallery.insertAdjacentHTML('beforeend', galleryMarkup);
-     } catch (error) {
+    toHideLoadMoreBtn(data);
+  } catch (error) {
     Notify.failure('Сталася помилка');
   }
+}
+function toHideLoadMoreBtn(data) {
+  console.log(data.totalHits);
+  const shownPages = Math.floor(data.totalHits / 40);
+  refs.addMoreBtn.style.display =
+    shownPages < pixabay.params.page ? 'none' : 'block';
+  if (refs.addMoreBtn.style.display === 'none')
+    refs.notification.style.display = 'block';
 }
 
 async function renderData() {
@@ -35,12 +46,16 @@ async function renderData() {
         'Sorry, there are no images matching your search query. Please try again.'
       );
     else {
+      Notify.success(`Hooray! We found ${data.totalHits} images`);
+
       const galleryMarkup = markupAllCards(data);
+
       refs.gallery.insertAdjacentHTML('beforeend', galleryMarkup);
-      refs.addMoreBtn.style.display = 'block';
+      toHideLoadMoreBtn(data);
       refs.addMoreBtn.addEventListener('click', onLoadMoreBtnClick);
     }
   } catch (error) {
     Notify.failure('Сталася помилка');
+    console.log(error);
   }
 }
--- src/js/refs.js
+++ src/js/refs.js
@@ -2,4 +2,5 @@ export default {
     gallery: document.querySelector('.gallery'),
     form: document.getElementById('search-form'),
     addMoreBtn: document.querySelector('.load-more'),
+    notification: document.querySelector('.notification'),
 }
